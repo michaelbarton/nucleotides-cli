118e2e94ec084d80492aa6b94a269b39d7317082
Michael Barton <mail@michaelbarton.me.uk>
2015-01-30 16:11:57 -0800

    Add S3 arguments to NCLE push event
    Signed-off-by: Michael Barton <mail@michaelbarton.me.uk>

--- features/ncle-push-event.feature
+++ features/ncle-push-event.feature
@@ -20,3 +20,32 @@ Feature: Fetching sequence data using ncle-fetch-data
     Then the stderr should not contain anything
      And the exit status should be 0
      And the output should match /^\d+$/
+
+  Scenario Outline: Running ncle-push-event with file arguments
+   Given the ncle directory is available on the path
+     And a file named "dummy-file" with:
+     """
+     dummy-contents
+     """
+    When I run the bash command:
+      """
+      ncle-push-event \
+        --benchmark-id="dummy" \
+        --benchmark-type-code="dummy" \
+        --status-code="dummy" \
+        --event-type-code="dummy" \
+        --s3-access-key=${AWS_ACCESS_KEY} \
+        --s3-secret-key=${AWS_SECRET_KEY} \
+        --s3-region="us-west-1" \
+        --s3-url="s3://nucleotid-es-dev/ncle-uploads/" \
+        --<argument>=dummy-file
+      """
+    Then the stderr should not contain anything
+     And the exit status should be 0
+     And the output should match /^\d+$/
+
+    Examples:
+      | argument    |
+      | event-file  |
+      | log-file    |
+      | cgroup-file |
--- lib/ncle/push_event.rb
+++ lib/ncle/push_event.rb
@@ -1,12 +1,18 @@
 require 'slop'
 require 'curl'
 
+require 'ncle/util'
+
 URL = 'http://api.nucleotid.es/events/update'
-REQUIRED_OPTS = [:benchmark_id, :benchmark_type_code, :status_code, :event_type_code]
 
 module NCLE
   module PushEvent
     class << self
+
+      def missing_options(opts)
+        required = [:benchmark_id, :benchmark_type_code, :status_code, :event_type_code]
+        NCLE::Util.missing_options(required, opts)
+      end
 
       def execute!
         opts = options
@@ -22,16 +28,21 @@ module NCLE
         missing_options(opts).empty?
       end
 
-      def missing_options(opts)
-        opts.select{|k,v| v.nil?}.keys
-      end
-
       def options
         opts = Slop.parse do |o|
           o.string '-i', '--benchmark-id',        'The benchmark this event corresponds to'
           o.string '-t', '--benchmark-type-code', 'The type of benchmark'
           o.string '-s', '--status-code',         'The outcome of this event'
           o.string '-c', '--event-type-code',     'The type of event'
+
+          o.string '-a', '--s3-access-key',       'AWS access key'
+          o.string '-x', '--s3-secret-key',       'AWS secret key'
+          o.string '-u', '--s3-url',              'AWS S3 file url'
+          o.string '-r', '--s3-region',           'AWS region'
+
+          o.string '-e', '--event-file',          'Path to event file to upload'
+          o.string '-l', '--log-file',            'Path to log file to upload'
+          o.string '-g', '--cgroup-file',         'Path to cgroup log file to upload'
         end
         Hash[opts.to_hash.map{|k,v| [k.to_s.gsub('-','_').to_sym, v]}]
       end
