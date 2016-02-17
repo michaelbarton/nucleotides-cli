ed21cffa7a3950774eec6f4158679720b94e1ae1
Michael Barton <mail@michaelbarton.me.uk>
2016-02-16 18:05:43 -0800

    Implement creating metadata.json file

--- nucleotides/command/fetch_data.py
+++ nucleotides/command/fetch_data.py
@@ -5,7 +5,21 @@ Usage:
     nucleotides fetch-data <task>
 """
 
-import nucleotides.util as util
+import nucleotides.util        as util
+import nucleotides.api_client  as api
+import biobox_cli.util.misc    as bbx_util
 
 def run(args):
-    util.parse(__doc__, args)
+    opts = util.parse(__doc__, args)
+    create_metadata_file(opts["<task>"])
+
+def create_metadata_file(metadata):
+    import json
+    path = "nucleotides-task/{}".format(metadata["id"])
+    bbx_util.mkdir_p(path)
+    with open(path + "/metadata.json", "w") as f:
+        f.write(json.dumps(metadata))
+
+def create_benchmark_dir(id_):
+    metadata = api.fetch_task(id_)
+    create_metadata_file(metadata)
--- test/test_fetch_data.py
+++ test/test_fetch_data.py
@@ -1,3 +1,4 @@
+import os, shutil
 import nose.tools as nose
 
 import nucleotides.command.fetch_data as fetch
@@ -6,3 +7,9 @@ import nucleotides.util               as util
 def test_docstring_parse():
     nose.assert_equal(util.parse(fetch.__doc__, ["fetch-data", "1"]),
             {'<task>': '1', 'fetch-data': True})
+
+def test_create_benchmark_dir():
+    fetch.create_benchmark_dir("1")
+    exists = os.path.isfile("nucleotides-task/1/metadata.json")
+    shutil.rmtree("nucleotides-task")
+    nose.assert_true(exists)
