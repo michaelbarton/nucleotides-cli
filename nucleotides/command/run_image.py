5925604e83bb58ebdea7736890bb0d4249bf1820
Michael Barton <mail@michaelbarton.me.uk>
2016-02-22 11:21:14 -0800

    Docstring parse in run-image command

--- nucleotides/command/run_image.py
+++ nucleotides/command/run_image.py
@@ -1,2 +1,9 @@
+"""
+nucleotides run-image - Execute Docker image for benchmarking
+
+Usage:
+    nucleotides run-image <task>
+"""
+
 def run(args):
     None
--- test/test_run_image.py
+++ test/test_run_image.py
@@ -2,4 +2,9 @@ import os.path
 import nose.tools as nose
 import helper
 
+import nucleotides.util              as util
 import nucleotides.command.run_image as image
+
+def test_docstring_parse():
+    nose.assert_equal(util.parse(image.__doc__, ["run-image", "1"]),
+            {'<task>': '1', 'run-image': True})
