d1ddb401d6c8844b6fa9ac4bb27e80406429448e
Michael Barton <mail@michaelbarton.me.uk>
2016-02-24 11:05:26 -0800

    Add function for uploading S3 files

--- nucleotides/s3.py
+++ nucleotides/s3.py
@@ -1,9 +1,18 @@
-import boto3, urlparse
-import boto3.s3.transfer as trn
+def s3():
+    import boto3, boto3.s3.transfer
+    return boto3.s3.transfer.S3Transfer(boto3.client('s3'))
+
+def parse_s3_url(url):
+    import urlparse
+    location = urlparse.urlparse(url)
+    _, key = location.path.split('/', 1)
+    bucket = location.netloc
+    return [bucket, key]
 
 def fetch_file(src, dst):
-    s3 = trn.S3Transfer(boto3.client('s3'))
-    location = urlparse.urlparse(src)
-    _, file_ = location.path.split('/', 1)
-    bucket   = location.netloc
-    s3.download_file(bucket, file_, dst)
+    bucket, key = parse_s3_url(src)
+    s3().download_file(bucket, key, dst)
+
+def post_file(src, dst):
+    bucket, key = parse_s3_url(dst)
+    s3().upload_file(src, bucket, key)
--- test/test_s3.py
+++ test/test_s3.py
@@ -1,10 +1,29 @@
-import tempfile, os.path
+import tempfile, os.path, boto3
 
 import nose.tools     as nose
 import nucleotides.s3 as s3
+
+def assert_s3_file_exists(bucket_name, path):
+    bucket = boto3.resource('s3').Bucket(bucket_name)
+    objs = list(bucket.objects.filter(Prefix=path))
+    if not (len(objs) > 0 and objs[0].key == path):
+        nose.assert_true(False, "File not found: s3://{}/{}".format(bucket_name, path))
+
+def delete_s3_file(bucket_name, key):
+    boto3.client('s3').delete_object(Bucket=bucket_name, Key=key)
 
 def test_fetch_file():
     src = "s3://nucleotides-testing/short-read-assembler/reference.fa"
     dst = os.path.join(tempfile.mkdtemp(), 'reference.fa')
     s3.fetch_file(src, dst)
     nose.assert_true(os.path.isfile(dst))
+
+def test_post_file():
+    import time
+    f_name = "nucleotides_client_testing-{}".format(int(round(time.time() * 1000)))
+    bucket = "nucleotides-testing"
+    dst = "s3://{}/{}".format(bucket, f_name)
+    src = tempfile.mkstemp()[1]
+    s3.post_file(src, dst)
+    assert_s3_file_exists(bucket, f_name)
+    delete_s3_file(bucket, f_name)
