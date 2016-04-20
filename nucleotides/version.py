43047950ac70bed47228edbc007f97d19b6eaa5f
Michael Barton <mail@michaelbarton.me.uk>
2016-04-20 16:02:27 -0700

    Fetch version number using package resources

--- nucleotides/version.py
+++ nucleotides/version.py
@@ -1,8 +1,7 @@
-import os
+import pkg_resources, os
 
 def get_version():
-    version_file = os.path.join(os.path.dirname(__file__), '../VERSION')
-    with open(version_file) as f:
-        return f.read().strip()
+    path = os.path.join('..', 'VERSION')
+    return pkg_resources.resource_string(__name__, path).strip()
 
 __version__ = get_version()
