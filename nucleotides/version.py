cf9f3c223214e74765e32f2a41f5a9ec1d5b38fa
Michael Barton <mail@michaelbarton.me.uk>
2016-04-20 17:42:11 -0700

    Hardcode verson number into python code

--- nucleotides/version.py
+++ nucleotides/version.py
@@ -1,7 +1,1 @@
-import pkg_resources, os
-
-def get_version():
-    path = os.path.join('..', 'VERSION')
-    return pkg_resources.resource_string(__name__, path).strip()
-
-__version__ = get_version()
+__version__ = '0.1.0'
