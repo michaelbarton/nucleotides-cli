bd20565bd931e43da3433e4650f46b3338e02bd1
Michael Barton <mail@michaelbarton.me.uk>
2016-09-07 14:36:25 +0100

    Parse additional metrics from groups using jmespath

--- nucleotides/metrics.py
+++ nucleotides/metrics.py
@@ -6,31 +6,26 @@ the Docker container into metrics that may be uploaded to the nucleotides API.
 """
 Interval in seconds in which cgroup data is collected
 """
-SAMPLING_INTERVAL = 15
+SAMPLING_INTERVAL      = 15
+BYTE_TO_MIBIBYTE       = 1.0 / 1024 ** 2
+NANOSECONDS_TO_SECONDS = 1e-9
 
-def remap_cgroup_values(a):
-    """
-    Remaps the raw cgroup data paths into the metric names used by nucleotides.
-    """
-    return {"max_memory_usage" : a["memory_stats"]["max_usage"],
-            "max_cpu_usage"    : a["cpu_stats"]["cpu_usage"]["total_usage"]}
-
-
-# http://stackoverflow.com/a/25658642/91144
-def max_values(a, b):
-    """
-    For two dictionaries a & b, returns a single dictionary containing the maximum
-    value for each key in the dictionaries.
-    """
-    return {key: max(value, b[key]) for key, value in a.iteritems()}
-
+CGROUP_JMESPATHS = {
+    "total_memory_usage_in_mibibytes"          : ["max([*].memory_stats.max_usage)", BYTE_TO_MIBIBYTE],
+    "total_rss_in_mibibytes"                   : ["max([*].memory_stats.stats.total_rss)", BYTE_TO_MIBIBYTE],
+    "total_cpu_usage_in_seconds"               : ["max([*].cpu_stats.cpu_usage.total_usage)", NANOSECONDS_TO_SECONDS],
+    "total_cpu_usage_in_seconds_in_kernelmode" : ["max([*].cpu_stats.cpu_usage.usage_in_kernelmode)", NANOSECONDS_TO_SECONDS],
+    "total_cpu_usage_in_seconds_in_usermode"   : ["max([*].cpu_stats.cpu_usage.usage_in_usermode)", NANOSECONDS_TO_SECONDS],
+    "total_read_io_in_mibibytes"               : ["max([*].sum(blkio_stats.io_service_bytes_recursive[?op=='Read'].value))", BYTE_TO_MIBIBYTE],
+    "total_write_io_in_mibibytes"              : ["max([*].sum(blkio_stats.io_service_bytes_recursive[?op=='Write'].value))", BYTE_TO_MIBIBYTE] }
 
 def parse_runtime_metrics(metrics):
     """
     Given a list of cgroup dictionaries, parses them into a single dictionary of
     nucleotides metrics that can be uploaded to the nucleotides API.
     """
-    nucleotides_metrics = reduce(max_values, map(remap_cgroup_values, metrics))
+    import jmespath
+    f = lambda (name, (path, units)): [name, round(jmespath.search(path, metrics) * units, 3)]
+    nucleotides_metrics = dict(map(f, CGROUP_JMESPATHS.iteritems()))
     nucleotides_metrics["total_wall_clock_time_in_seconds"] = len(metrics) * SAMPLING_INTERVAL
     return nucleotides_metrics
-
--- test/test_metrics.py
+++ test/test_metrics.py
@@ -1,8 +1,25 @@
 import nose.tools          as nose
 import nucleotides.metrics as met
 
+from nose.plugins.attrib import attr
+
+@attr('wip')
 def test_parse_runtime_metrics():
-    inputs = [{"memory_stats": {"max_usage" : 10}, "cpu_stats" : {"cpu_usage" : {"total_usage" : 40}}},
-              {"memory_stats": {"max_usage" : 20}, "cpu_stats" : {"cpu_usage" : {"total_usage" : 80}}}]
-    outputs = {"max_memory_usage" : 20, "max_cpu_usage" : 80, "total_wall_clock_time_in_seconds" : 30}
-    nose.assert_equal(met.parse_runtime_metrics(inputs), outputs)
+    import gzip, json
+    with gzip.open('data/cgroup_metrics.json.gz') as f:
+        metrics = met.parse_runtime_metrics(json.loads(f.read()))
+
+    expected = {
+        "total_cpu_usage_in_seconds"               : 53.546,
+        "total_cpu_usage_in_seconds_in_kernelmode" : 1.75,
+        "total_cpu_usage_in_seconds_in_usermode"   : 11.11,
+        "total_memory_usage_in_mibibytes"          : 175.348,
+        "total_rss_in_mibibytes"                   : 80.543,
+        "total_read_io_in_mibibytes"               : 38.641,
+        "total_write_io_in_mibibytes"              : 0.0,
+        "total_wall_clock_time_in_seconds"         : 15.0}
+
+    for k,v in expected.iteritems():
+        nose.assert_in(k, metrics)
+        nose.assert_equal(metrics[k], v,
+                "Exected {} to equal {} but was {}.".format(k, v, metrics[k]))
