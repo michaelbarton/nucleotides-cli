407c7d3a4182b68a6dd13e12f1d522f0b2d2f99f
Michael Barton <mail@michaelbarton.me.uk>
2017-01-10 14:22:22 -0800

    Add function to copy container output files

--- test/helper/application.py
+++ test/helper/application.py
@@ -68,13 +68,13 @@ def mock_reference_evaluator_state(inputs = True, intermediates = False, outputs
         copy_to_file('tmp/data/contigs.fa', 'inputs/contig_fasta/7e9f760161.fa', app)
 
     if intermediates:
-        copy_to_file('data/log.txt', 'meta/log.txt', app)
+        copy_to_file('data/log.txt', 'tmp/meta/log.txt', app)
         copy_to_file('tmp/data/assembly_metrics.tsv', 'tmp/combined_quast_output/report.tsv', app)
         copy_to_file('data/quast_biobox.yaml', 'tmp/biobox.yaml', app)
 
     if outputs:
-        copy_to_file('tmp/data/assembly_metrics.tsv', 'outputs/assembly_metrics/outputs.csv', app)
-        copy_to_directory('data/log.txt',             'outputs/container_log', app)
-        copy_to_file('data/cgroup_metrics.json.gz', 'outputs/container_runtime_metrics/metrics.json.gz', app)
+        copy_to_file('tmp/data/assembly_metrics.tsv', 'outputs/assembly_metrics/67ba437ffa', app)
+        copy_to_file('data/log.txt',                  'outputs/container_log/e0e8af3790', app)
+        copy_to_file('data/cgroup_metrics.json.gz',   'outputs/container_runtime_metrics/metrics.json.gz', app)
 
     return app
--- test/test_filesystem.py
+++ test/test_filesystem.py
@@ -1,3 +1,4 @@
+import os.path
 import nose.tools             as nose
 import helper.application     as app_helper
 import nucleotides.filesystem as fs
@@ -8,3 +9,21 @@ def test_get_output_biobox_file_contents():
     app = app_helper.mock_short_read_assembler_state(intermediates = True)
     args = fs.get_output_biobox_file_contents(app)
     nose.assert_in('fasta', args[0])
+
+
+def test_copy_container_output_files_with_no_files():
+    app = app_helper.mock_short_read_assembler_state(intermediates = False)
+    input_files = {'container_log' : 'meta/log.txt', 'assembly_metrics' : 'combined_quast_output/report.tsv'}
+    fs.copy_container_output_files(app, input_files)
+    # Should do nothing if files don't exist
+
+
+def test_copy_container_output_files_with_intermediates():
+    app = app_helper.mock_reference_evaluator_state(intermediates = True)
+    input_files = {'container_log' : 'meta/log.txt', 'assembly_metrics' : 'combined_quast_output/report.tsv'}
+    output_files = ['assembly_metrics/67ba437ffa', 'container_log/e0e8af3790']
+
+    fs.copy_container_output_files(app, input_files)
+    for f in output_files:
+        loc = fs.get_task_file_path(app, "outputs/" + f)
+        assert os.path.isfile(loc), "Output file should be copied: {}".format(loc)
